288abd2545771b8e38bc20ee7e16ceb44a01f35d
OdeyemiFavour <Odeyemifavour593@gmail.com>
2025-01-01 19:52:34 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+  import HomePage from './views/HomePage.vue';
+  
+</script>
+<template>
+  <h1>hello vue</h1>
+  <HomePage/>
+</template>
+<style scoped></style>
+
---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+
+<template>
+    <div>
+      <h1>Welcome to the Home Page</h1>
+      <p>This is the root of your application.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
